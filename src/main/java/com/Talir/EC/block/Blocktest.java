91caf1df39e64bd56c2365c72a74cb7c3faad5c6
Karsten <Karsten_Ernert@yahoo.de>
2015-07-21 21:10:50 +0200

    Erster Upload, leider ohne Texturen

---
+++ src/main/java/com/Talir/EC/block/Blocktest.java
@@ -0,0 +1,12 @@
+package com.Talir.EC.block;
+
+/**
+ * Created by Karsten on 21.07.2015.
+ */
+public class Blocktest extends BlockEC{
+    public Blocktest(){
+        super();
+        this.setBlockName("test");
+        this.setHardness(2.5f);
+    }
+}
